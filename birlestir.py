0e9ab1340c860cd6bb6d028405475c870b33d968
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2023-07-23 21:58:54 +0300

    Update birlestir.py

--- birlestir.py
+++ birlestir.py
@@ -9,7 +9,8 @@ import os
 # ilk_dosya_numarasi = int(input("İlk dosya numarasını yazın. Dosya numaraları sıralı olmalıdır: "))
 
 dosyalar = os.listdir()     # "birlestir.py" dosyasının bulunduğu dizindeki tüm dosya isimlerini, uzantıları ile birlikte al, "dosyalar" isimli değişkene ata.
- 
+dosyalar.sort()             # dosya isimlerini sırala
+
 dosya_isimleri= []      # ".xlsx" ya da ".xls" uzantılı dosyaların toplanacağı boş liste oluştur.
 
 for i in dosyalar:      # Dizindeki tüm dosya isimlerini kontrol et, ".xlsx" ya da ".xls" uzantılı dosyaları "dosya_isimleri" isimli listeye ekle.
@@ -19,15 +20,27 @@ for i in dosyalar:      # Dizindeki tüm dosya isimlerini kontrol et, ".xlsx" ya
 def VeriCercevesi(dosya_adi):   # Belirtilen dosya adına göre, dosya içeriğini DataFrame'e çeviren fonksiyon.
     return pd.read_excel(dosya_adi)
 
+baslik = VeriCercevesi(dosya_isimleri[0]).columns       # ilk dosyanın basligi
+
+def VeriCercevesiBasliksiz(dosya_adi):   # Belirtilen dosya adına göre, dosya içeriğini DataFrame'e çeviren fonksiyon.
+    return pd.read_excel(dosya_adi, header = None, names = baslik, skiprows=[0])  # dosya adındaki başlık silinecek, klasördeki ilk dosyanın başlığı tüm diğer dosyalara başlık olarak eklenecek.
+
 """
 Veri çerçevesi oluşturuken, dosya ismini de bir sütuna yazdır.
 """
 
-df = pd.DataFrame()
-
 for dosya in dosya_isimleri:
-    df_dosya = VeriCercevesi(dosya)
-    print(df_dosya.head())
+
+    if dosya == dosya_isimleri[0]:
+        df = VeriCercevesi(dosya)
+        # print(df)
+
+    else:
+        df_dosya = VeriCercevesiBasliksiz(dosya)
+        df.join(df_dosya)
+
+
+    # print(dosya, df_dosya.head())
     # df.join(df_dosya)
 
 
